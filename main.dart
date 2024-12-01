49fc0c12dcdecf9ecccc32b4e40332c16d660cd2
Cavinee <roderich.chow@binus.ac.id>
2024-12-01 19:22:05 +0700

    Pertemuan-3

--- main.dart
+++ main.dart
@@ -9,6 +9,34 @@ class Item {
   }
 }
 
+/*
+4 Pillars of Object Oriented Programming (OOP):
+1. Encapsulation = membungkus attribut dan method ke dalam 1 "kapsul" yaitu class untuk merahasiakan data dan mencegah akses yang tidak diinginkan (seperti melihat atau mengubah data).
+Dapat dicapai dengan mengggunakan access modifier, seperti private ('_') dan public
+
+Jika sebuah data diberikan private access modifier, maka untuk melihat dan mengubah data tersebut perlu dibuat method khusus, yaitu getter untuk melihat dan setter untuk mengubah data 
+
+2. Inheritance = mewariskan sifat atau ciri khas (atribut dan method) dari parent class (super class) ke child class (sub class) agar code kalian bisa menjadi reusable dan lebih modular
+Dapat dicapai dengan menggunakan keyword extends
+
+3. Polymorphism = bagaimana sebuah object bisa bersifat layaknya class-class lain (seperti bagaimana kita manusia bisa "berubah bentuk" menjadi mahasiswa, anak, orang tua, dan lain-lain) sehingga code kalian bisa menjadi lebih fleksibel
+Dapat dicapai dengan menggunakan method overriding (override method dari interface yang diimplementasikan ataupun method dari parent class)
+
+4. Abstraction = menyembunyikan detail implementasi dari program kalian, hanya menampilkan detail implementasi yang diperlukan oleh user sehingga user bisa berfokus kepada implementasi yang penting bagi program kita yang complex dapat dimengerti oleh pengguna kita
+Dapat dicapai dengan menggunakan keyword abstract pada sebuah class untuk membuat abstract class ataupun bisa dengan membuat abstract method (method yang belum ada implementasinya)
+
+Jika abstract method dimiliki oleh class normal (bukan abstract), maka method tersebut harus di override (method overriding) di mana implementasi dari method tersebut harus ditambahkan
+
+Bonus information:
+Pada Dart, inheritance hanya bisa dilakukan sekali saja karena bisa terjadi tabrakan antara data yang diturunkan.
+
+Alternatif dari multiple inheritance adalah kita bisa menggunakan interface untuk melakukan mulitple implementation dari berbagai interface
+
+Perbedaan dari interface dan class normal untuk inheritance adalah:
+- Inheritance hanya boleh sekali sedangkan implementation bisa berulang-ulang
+- Inheritance menggunakan keyword extends implementation menggunakan keyword implements
+*/
+
 // Inheritance: pewarisan sifat dari parent class ke sub class, dapat dilihat dari keyword extends
 // Polymorphism: object bisa memiliki banyak bentuk, dapat dilihat dari implementasi @override, di mana dilakukan method overriding
 class Food extends Item {
